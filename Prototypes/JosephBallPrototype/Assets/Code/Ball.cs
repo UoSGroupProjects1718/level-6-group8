c297f1cd878c1292dd34312a6b32c913b5576764
josephbarber96 <Joseph_Barber@Hotmail.co.uk>
2017-09-29 11:25:50 +0100

    Minor start on a prototype.

---
+++ Prototypes/JosephBallPrototype/Assets/Code/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public enum BallColour
+    {
+        Yellow,
+        Red,
+        Green,
+        Blue,
+        Purple
+    }
+
+    [SerializeField]
+    BallColour colour;
+
+    public BallColour GetBallColour() { return colour; }
+
+	void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
