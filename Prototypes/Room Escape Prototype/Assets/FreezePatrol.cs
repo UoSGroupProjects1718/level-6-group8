317acbca7b5aa869291750ffba278048e26af099
johnydorman <Johndorman0@gmail.com>
2017-10-09 20:02:04 +0100

    Room escape prototype

---
+++ Prototypes/Room Escape Prototype/Assets/FreezePatrol.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezePatrol : MonoPause {
+
+    public Transform[] waypoints;
+    public float speed = 0.5f;
+
+    private int index;
+    private Transform currentWaypoint;
+
+    // Use this for initialization
+    protected override void DoStart () {
+        index = 0;
+        currentWaypoint = waypoints[index];
+    }
+
+    // Update is called once per frame
+    protected override void DoUpdate () {
+        if (currentWaypoint != null)
+        {
+            move();
+        }
+        else
+        {
+            nextWaypoint();
+        }
+    }
+
+    void move()
+    {
+        Vector3 direction = currentWaypoint.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+        if (direction.magnitude < 2.0f)
+        {
+            Debug.Log("fuaoigbal");
+            currentWaypoint = null;
+        }
+    }
+
+    void nextWaypoint()
+    {
+        index++;
+        if (index == waypoints.Length)
+        {
+            index = 0;
+        }
+        currentWaypoint = waypoints[index];
+    }
+}
