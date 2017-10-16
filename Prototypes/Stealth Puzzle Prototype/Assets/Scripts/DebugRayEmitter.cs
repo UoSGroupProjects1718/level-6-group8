c9604600f98ce01fe00e78f59fdc82e4f46ce6a4
Elliot <elliot+github@dewhurst.me>
2017-10-16 17:11:36 +0100

    Added output research

---
+++ Prototypes/Stealth Puzzle Prototype/Assets/Scripts/DebugRayEmitter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugRayEmitter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay(transform.position, transform.forward * 100, Color.green);
+	}
+}
