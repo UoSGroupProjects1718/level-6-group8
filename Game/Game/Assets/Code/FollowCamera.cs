db38c3690b8226e3520fcdc4ee45a85bb3b32161
johnydorman <Johndorman0@gmail.com>
2017-11-13 02:01:16 +0000

    UI rotation, temp art, and ui panel scaling

---
+++ Game/Game/Assets/Code/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public Vector3 cameraPos;
+    private float strength = 10.5f;
+
+	// Update is called once per frame
+	void Update () {
+       // cameraPos = Camera.main.transform.position;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Camera.main.transform.position - transform.position), Mathf.Min(strength * Time.deltaTime, 1));
+    }
+}
---
+++ Game/Game/Assets/Code/MoveCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    // Speed of drag
+    public float dragSpeed = 1;
+
+    // Movement Vector
+    private Vector3 move;
+    // Origin of each drag
+    private Vector3 dragOrigin;
+
+    // May be neeed later for limit drag movement 
+    //private Vector3 posOrigin;
+
+    void Update()
+    {
+        // If left mouse button was clicked this frame
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        // If the left mouse button is not being held
+        if (!Input.GetMouseButton(0)) return;
+        ///TODO: Make it check for input.touch instead of input.getmousebutton
+
+        // Set a new direction vector
+        Vector3 direction = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+
+        // Set Movement vector with speed
+        move = new Vector3(direction.x * dragSpeed,0 , direction.y * dragSpeed);
+        // Adjust for camera Angle
+        /// TODO: make this use transform.rotation
+        move = Quaternion.AngleAxis(0, Vector3.up) * move;
+
+        // Move the Camera
+        transform.Translate(-move, Space.World);
+    }
+}
