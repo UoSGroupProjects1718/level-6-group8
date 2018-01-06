d086adbee079220b23d59cfd7e4fa8fb60e13351
Elliot <elliot+github@dewhurst.me>
2018-01-06 12:16:23 +0000

    firebase database plugin added

---
+++ Game/Game/Assets/Code/HighscoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+
+public class HighscoreManager : MonoBehaviour
+{
+    private DatabaseReference db;
+	// Use this for initialization
+	void Start () {
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://group8-game.firebaseio.com/");
+	    db = FirebaseDatabase.DefaultInstance.RootReference;
+        TestWrite();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TestWrite()
+    {
+        db.Child("Testing").SetValueAsync("TestString");
+        db.Child("Testing").SetValueAsync(82529);
+
+        var dict = new Dictionary<string, object>();
+        dict.Add("test", 20200);
+        dict.Add("TestString", "BLA");
+
+        db.Child("Testing").SetValueAsync(dict);
+    }
+
+    void TestRead()
+    {
+        
+    }
+}
